b02cc844ef6054552c2b0b45299e44c01ada1df5
pirat <pirat@DESKTOP-U375SCL.local>
2020-12-03 18:53:30 -0800

    Update CUSTOMSCRIPTS.meta, Character.unity, and 10 more files...

---
+++ Assets/Scripts/WorldText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldText : MonoBehaviour
+{
+    // Create Text in the world
+    public static TextMesh WorldText (string text,Transform parent=null,Vector3 localPosition = default(Vector3),int fontSize=40,Color 0f)
+    {
+
+    }
+}
