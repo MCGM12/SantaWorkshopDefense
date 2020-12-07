a88bd068c218338eb65e687d03c7d54e79f76669
MCGM12 <matocraft506@gmail.com>
2020-12-07 12:43:54 -0800

    Update CUSTOMSCRIPTS.meta, AITest.unity, and 3 more files...

--- Assets/Scripts/WorldText.cs
+++ Assets/Scripts/WorldText.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class WorldText : MonoBehaviour
 {
     // Create Text in the world
-    public static TextMesh WorldText (string text,Transform parent=null,Vector3 localPosition = default(Vector3),int fontSize=40,Color 0f)
-    {
+    //public static TextMesh WorldText (string text,Transform parent=null,Vector3 localPosition = default(Vector3),int fontSize=40,Color 0f)
+    //{
 
-    }
+    //}
 }
