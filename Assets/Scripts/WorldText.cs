e95aab0a52f809b12f634a5bc51b20f70e4551fc
pirat <pirat@DESKTOP-U375SCL.local>
2020-12-07 20:00:01 -0800

    Update CUSTOMSCRIPTS.meta, Script.meta, and 2 more files...

--- Assets/Scripts/WorldText.cs
+++ Assets/Scripts/WorldText.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WorldText : MonoBehaviour
+/*public class WorldText : MonoBehaviour
 {
     // Create Text in the world
-    //public static TextMesh WorldText (string text,Transform parent=null,Vector3 localPosition = default(Vector3),int fontSize=40,Color 0f)
-    //{
+  public static TextMesh CreateWorldText (Transform parent = null, string text, Vector3 localPosition = default, int fontSize = 40, Color color, 
+        TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = 5000)
+    {
+        GameObject gameObject = new GameObject("world_Text",typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.alignment = textAlignment;
+        textMesh.text = text;
+        textMesh.fontSize = fontSize;
+       textMesh.color = color;
+        textMesh.GetComponent<MeshRenderer>().sortingOrder=sortingOrder;
+        return textMesh;
 
-    //}
-}
+    }
+    
+}*/
