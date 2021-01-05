a251765029981277f913c4af1b7e18d80535b660
MCGM12 <matocraft506@gmail.com>
2021-01-05 12:38:31 -0800

    Update Background.prefab, Background.prefab.meta, and 3 more files...

---
+++ Assets/RunnerAttack.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunnerAttack : MonoBehaviour
+{
+    public GameObject[] gos;
+
+    public void Start()
+    {
+        
+    }
+    public void Update()
+    {
+        
+    }
+
+    public GameObject FindClosestEnemy()
+    {
+        gos = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+}
