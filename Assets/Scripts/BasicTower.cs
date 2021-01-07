14d8afd391f5ee62449444896802457dda4f94f3
pirat <pirat@DESKTOP-U375SCL.local>
2021-01-07 00:50:37 -0800

    Update CUSTOMSCRIPTS.meta, SnowballTower.prefab, and 7 more files...

--- Assets/Scripts/BasicTower.cs
+++ Assets/Scripts/BasicTower.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class BasicTower : SnowballTower
 {
+    public Transform pivot;
+    public Transform barrel;
     public GameObject Snowball;
     protected override void Shoot()
     {
         base.Shoot();
 
-        GameObject newSnowball = Instantiate(Snowball);
+        GameObject newSnowball = Instantiate(Snowball,barrel.position,pivot.rotation);
+        
 
         Debug.Log("Shoot");
     }
