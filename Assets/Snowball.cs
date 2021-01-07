aade32a2a60fd814f98ade393979e03910b88eb0
pirat <pirat@DESKTOP-U375SCL.local>
2021-01-07 03:43:03 -0800

    Update SnowBall.prefab, SnowballTower.prefab, and 8 more files...

---
+++ Assets/Snowball.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowball : MonoBehaviour
+{
+    private void Update()
+    {
+        
+        transform.position += transform.right * 0.25f;
+
+        Debug.Log("KOBE");
+    }
+}
