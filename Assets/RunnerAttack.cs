24da082afc7cc2420d7dc6f89c2accebd401e9a3
MCGM12 <matocraft506@gmail.com>
2021-01-07 00:51:52 -0800

    Update CUSTOMSCRIPTS.meta, Basic Runner.prefab, and 4 more files...

--- Assets/RunnerAttack.cs
+++ Assets/RunnerAttack.cs
@@ -4,20 +4,44 @@ using System.Collections;
 public class RunnerAttack : MonoBehaviour
 {
     public GameObject[] gos;
+    public float projSpeed;
+    public static GameObject projectile;
+    public GameObject closestEnemy;
+    public bool shoot1, shoot2, shoot3; //shoot1 is distance req, shoot2 is timer req, and shoot3 is if all reqs are satisfied
+    private GameObject closest;
+   
 
     public void Start()
     {
-        
+        projSpeed = 2f;
     }
     public void Update()
     {
-        
+        closestEnemy = closest;
+        FindClosestEnemy();
+        closestEnemy = closest;
+
+
+
+
+        if (Vector2.Distance(this.transform.position, closestEnemy.transform.position) < 10)
+        {
+            Debug.Log("Elf Tower in sight. Sending an Alma.");
+            shoot1 = true;
+        }
+
+        if(shoot3)
+        {
+            GameObject newProj = GameObject.Instantiate(projectile, transform.position, Quaternion.Euler(0, 0, 0));
+            newProj.transform.position = Vector2.MoveTowards(newProj.transform.position, closestEnemy.transform.position, 20);
+
+        }
     }
 
     public GameObject FindClosestEnemy()
     {
-        gos = GameObject.FindGameObjectsWithTag("Enemy");
-        GameObject closest = null;
+        gos = GameObject.FindGameObjectsWithTag("Player");
+        closest = null;
         float distance = Mathf.Infinity;
         Vector3 position = transform.position;
         foreach (GameObject go in gos)
