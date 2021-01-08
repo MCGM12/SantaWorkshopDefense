c4730abba0c8f7d853e10e738a77f44e4bff9d0e
pirat <pirat@DESKTOP-U375SCL.local>
2021-01-07 16:46:09 -0800

    Update FastShooter.prefab, FastShooter.prefab.meta, and 5 more files...

--- Assets/Snowball.cs
+++ Assets/Snowball.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Snowball : MonoBehaviour
 {
+    private void Start()
+    {
+        Destroy(gameObject, 3f);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        Debug.Log("Get Sniped ");
+    }
     private void Update()
     {
         
