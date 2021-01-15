c1a39e0372d4d1dbb00c0f1617723f6285ed4b38
MCGM12 <matocraft506@gmail.com>
2021-01-14 21:59:20 -0800

    Update AITest2.unity, Credits.unity, and 7 more files...

---
+++ Assets/Scripts/CreditRoller.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditRoller : MonoBehaviour
+{
+    private static int nScreens = 7;
+    private GameObject[] creditScreens = new GameObject[nScreens];
+    private static int swapCount = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        creditScreens[0] = GameObject.Find("Credits1");
+        creditScreens[1] = GameObject.Find("Credits2");
+        creditScreens[2] = GameObject.Find("Credits3");
+        creditScreens[3] = GameObject.Find("Credits4");
+        creditScreens[4] = GameObject.Find("Credits5");
+        creditScreens[5] = GameObject.Find("Credits6");
+        creditScreens[6] = GameObject.Find("Credits7");
+
+
+
+
+        //Turn them all off...
+        for (int i = 0; i < nScreens; i++)
+        {
+            creditScreens[i].SetActive(false);
+        }
+        //Except, turn back on element 0
+        creditScreens[0].SetActive(true);
+    } //Start
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            //Toggle
+            int currentScene = swapCount % nScreens;
+            creditScreens[currentScene].SetActive(false);
+            swapCount++;
+            currentScene = swapCount % nScreens;
+            creditScreens[currentScene].SetActive(true);
+            Debug.Log(currentScene);
+
+
+        }
+    } //Update
+}
---
+++ Assets/UISceneSwitcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UISceneSwitcher : MonoBehaviour
+{
+    public void Credits()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void Level()
+    {
+        SceneManager.LoadScene("AITest2");
+    }
+
+}
