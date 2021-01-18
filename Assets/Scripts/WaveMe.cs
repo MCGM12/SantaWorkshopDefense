454e485700fba35766f0515feb364248d5bab664
MCGM12 <matocraft506@gmail.com>
2021-01-18 14:12:00 -0800

    Update WaveManagerMe.cs, WaveManagerMe.cs.meta, and 164 more files...

---
+++ Assets/Scripts/WaveMe.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMe : MonoBehaviour
+{
+    [SerializeField] public static GameObject runnerPreset, gumballPreset, monsterPreset;
+    public static Transform origin;
+    public static GameObject waveManager;
+
+    public int EnemyCount;
+    public int runnerCount;
+    public int gumballCount;
+    public int monsterCount;
+    public bool ended;
+
+
+
+
+    public void Awake()
+    {
+        waveManager = GameObject.Find("WaveManager");
+        origin = waveManager.transform;
+        ended = false;
+        runnerPreset = GameObject.Find("Basic Runner");
+        gumballPreset = GameObject.Find("Basic Gumball");
+        monsterPreset = GameObject.Find("Monster Preset");
+        Debug.Log(" Awake Wave started");
+        
+    }
+}
---
+++ Assets/Scripts/WaveStuff/MoveEnemy.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * Copyright (c) 2017 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class MoveEnemy : MonoBehaviour
+{
+    public GameObject[] waypointDisplay;
+    [HideInInspector]
+    public GameObject[] waypoints;
+    private int currentWaypoint = 0;
+    private float lastWaypointSwitchTime;
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        lastWaypointSwitchTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waypointDisplay = waypoints;
+        // 1 
+        Vector3 startPosition = waypoints[currentWaypoint].transform.position;
+        Vector3 endPosition = waypoints[currentWaypoint + 1].transform.position;
+        // 2 
+        float pathLength = Vector2.Distance(startPosition, endPosition);
+        float totalTimeForPath = pathLength / speed;
+        float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
+        gameObject.transform.position = Vector2.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+        // 3 
+        if (gameObject.transform.position.Equals(endPosition)) //(Vector2.Distance(gameObject.transform.position, endPosition) <= 1)
+        {
+            if (currentWaypoint < waypoints.Length - 2) //3.a
+            {
+                // 4 Switch to next waypoint
+                currentWaypoint++;
+                Debug.Log("WAYPOINT SWITCHED");
+                lastWaypointSwitchTime = Time.time;
+                // TODO: Rotate into move direction
+            }
+            else //3.b
+            {
+                // 5 Destroy enemy
+                Destroy(gameObject);
+
+                //AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+                //AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+                // TODO: deduct health
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "FinalWaypoint")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
