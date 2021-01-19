356a013eabf25824bb2d9d759f4da8450f26b632
pirat <pirat@DESKTOP-U375SCL.local>
2021-01-18 22:53:52 -0800

    Update 1611015187792.png, 1611015187792.png.meta, and 22 more files...

---
+++ Assets/Scripts/TowerBnt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBnt : MonoBehaviour
+{
+    [SerializeField]
+    GameObject towerObject;
+    public GameObject TowerObject
+    {
+        get
+        {
+            return towerObject;
+        }
+    }
+}
---
+++ Assets/TowerManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TowerManager: MonoBehaviour
+{
+    TowerBnt towerBtnPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);// lets us see that a button was clicked 
+            RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.zero);// Lets us get information on what it hits or where tower will be placed
+            PlaceTower(hit);
+
+        }
+    }
+    public void PlaceTower(RaycastHit2D hit)
+    {
+        if(!EventSystem.current.IsPointerOverGameObject() && towerBtnPressed !=null )
+        {
+            GameObject newTower = Instantiate(towerBtnPressed.TowerObject);
+            newTower.transform.position = hit.transform.position;
+        }
+      
+    }
+    public void SelectedTower(TowerBnt towerSelected)// Lets me select the button and chosse a tower
+    {
+        towerBtnPressed = towerSelected;
+        Debug.Log("Pressed" + towerBtnPressed.gameObject);
+    }
+}
