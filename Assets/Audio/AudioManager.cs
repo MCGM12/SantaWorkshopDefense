0e191b24e7ffc3f90d08ce682d8dda1c14860f38
pirat <pirat@DESKTOP-U375SCL.local>
2021-01-20 13:12:05 -0800

    Update Audio.meta, 146730__leszek-szary__shoot.wav, and 7 more files...

---
+++ Assets/Audio/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource Click;
+    public AudioSource Shoot;
+    public AudioSource Die;
+
+    public void PlayClick()
+    {
+        Click.Play();
+    } 
+    public void PlayShoot()
+    {
+        Shoot.Play();
+    } 
+    public void PlayDie()
+    {
+        Die.Play();
+    }
+}
